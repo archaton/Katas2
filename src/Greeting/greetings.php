06bf8abea3687447b2837121c26b71de28422728
Gabriel Bernacki <gabriel.bernacki@gmail.com>
2021-03-23 08:34:17 +0100

    greetings test final

--- src/Greeting/greetings.php
+++ src/Greeting/greetings.php
@@ -4,7 +4,65 @@ declare(strict_types=1);
 
 namespace Katas\Greeting;
 
-function greet(string $name): string
+const GREET_TEMPLATE = 'Hello, %s.';
+const SHOUTING_GREET = 'HELLO %s!';
+
+function greet(?string ...$names): string
 {
-    return 'Hello, ' . $name;
+    if (count($names) === 1) {
+        $name = reset($names);
+
+        return handleOneName($name);
+    }
+    $names = array_reduce($names, static fn (array $acc, string $n):array => handleInput($acc, $n), []);
+    $uppercase = array_filter($names, static fn (string $n): bool => isAllUppercase($n));
+    $names = array_filter($names, static fn (string $n): bool => !isAllUppercase($n));
+    $greeting = '';
+
+    if (count($names) === 2) {
+        $greeting .= sprintf(GREET_TEMPLATE, implode(' and ', $names));
+    }
+    if (count($names) > 2) {
+        $lastName = array_pop($names);
+        array_push($names, 'and ' . $lastName);
+        $greeting .= sprintf(GREET_TEMPLATE, implode(', ', $names));
+    }
+    if (count($uppercase) > 0) {
+        $N = reset($uppercase);
+        $greeting .= sprintf(' AND ' . SHOUTING_GREET, $N);
+    }
+
+    return $greeting;
+}
+
+function handleInput(array $acc, string $n): array
+{
+    if (isEscaped($n)) {
+        $n = trim($n, '"');
+        return [...$acc, $n];
+    }
+    $ns = explode(', ', $n);
+    return [...$acc, ...$ns];
+}
+
+function isEscaped(string $n): bool
+{
+    return false !== mb_strpos($n, '"');
+}
+
+function handleOneName(?string $name): string
+{
+    if (null === $name) {
+        return sprintf(GREET_TEMPLATE, 'my friend');
+    }
+    if (isAllUppercase($name)) {
+        return sprintf(SHOUTING_GREET, $name);
+    }
+
+    return sprintf(GREET_TEMPLATE, $name);
+}
+
+function isAllUppercase(string $name): bool
+{
+    return mb_strtoupper($name) === $name;
 }
--- tests/GreetingTest.php
+++ tests/GreetingTest.php
@@ -12,19 +12,26 @@ use function Katas\Greeting\greet;
 class GreetingTest extends TestCase
 {
     /**
-     * @dataProvider provideValidNameCases
+     * @dataProvider provideGreetingCases
      */
-    public function testSuccessSimpleInterpolation(string $name)
+    public function testSuccessGreeting(string $expectedGreeting, ?string ...$name)
     {
-        $result = greet($name);
+        $result = greet(...$name);
 
-        $this->assertSame('Hello, '. $name, $result);
+        $this->assertSame($expectedGreeting, $result);
     }
 
-    public function provideValidNameCases(): array
+    public function provideGreetingCases(): array
     {
         return [
-            ['Bob'],
+            'Requirement 1' => ["Hello, Bob.", "Bob"],
+            'Requirement 2' => ["Hello, my friend.", null],
+            'Requirement 3' => ["HELLO JERRY!", "JERRY"],
+            'Requirement 4' => ["Hello, Jill and Jane.", "Jill", "Jane"],
+            'Requirement 5' => ["Hello, Amy, Brian, and Charlotte.", "Amy", "Brian", "Charlotte"],
+            'Requirement 6' => ["Hello, Amy and Charlotte. AND HELLO BRIAN!", "Amy", "BRIAN", "Charlotte"],
+            'Requirement 7' => ["Hello, Bob, Charlie, and Dianne.", "Bob", "Charlie, Dianne"],
+            'Requirement 8' => ["Hello, Bob and Charlie, Dianne.", "Bob", "\"Charlie, Dianne\""],
         ];
     }
 }
