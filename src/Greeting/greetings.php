a31918ae516ade1fb580515284e7942000c547e6
Gabriel Bernacki <gabriel.bernacki@gmail.com>
2021-03-26 08:50:04 +0100

    greetings test final

--- src/Greeting/greetings.php
+++ src/Greeting/greetings.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Katas\Greeting;
 
-const GREET_TEMPLATE = 'Hello, %s.';
-const SHOUTING_GREET = 'HELLO %s!';
-
 function greet(?string ...$names): string
 {
     if (count($names) === 1) {
@@ -14,52 +11,46 @@ function greet(?string ...$names): string
 
         return handleOneName($name);
     }
-    $names = array_reduce($names, static fn (array $acc, string $n):array => handleInput($acc, $n), []);
+    $names = array_reduce($names, static fn (array $acc, string $n): array => [...$acc, ...handleInput($n)], []);
     $uppercase = array_filter($names, static fn (string $n): bool => isAllUppercase($n));
     $names = array_filter($names, static fn (string $n): bool => !isAllUppercase($n));
-    $greeting = '';
-
+    $joinedNames = '';
     if (count($names) === 2) {
-        $greeting .= sprintf(GREET_TEMPLATE, implode(' and ', $names));
+        $joinedNames = implode(' and ', $names);
     }
     if (count($names) > 2) {
         $lastName = array_pop($names);
         array_push($names, 'and ' . $lastName);
-        $greeting .= sprintf(GREET_TEMPLATE, implode(', ', $names));
+        $joinedNames = implode(', ', $names);
     }
+
+    $greeting = "Hello, {$joinedNames}.";
     if (count($uppercase) > 0) {
-        $N = reset($uppercase);
-        $greeting .= sprintf(' AND ' . SHOUTING_GREET, $N);
+        $uppercaseName = reset($uppercase);
+        $greeting .= " AND HELLO {$uppercaseName}!";
     }
 
     return $greeting;
 }
 
-function handleInput(array $acc, string $n): array
+function handleInput(string $n): array
 {
-    if (isEscaped($n)) {
-        $n = trim($n, '"');
-        return [...$acc, $n];
+    if (false !== mb_strpos($n, '"')) {
+        return [trim($n, '"')];
     }
-    $ns = explode(', ', $n);
-    return [...$acc, ...$ns];
-}
-
-function isEscaped(string $n): bool
-{
-    return false !== mb_strpos($n, '"');
+    return explode(', ', $n);
 }
 
 function handleOneName(?string $name): string
 {
     if (null === $name) {
-        return sprintf(GREET_TEMPLATE, 'my friend');
+        return "Hello, my friend.";
     }
     if (isAllUppercase($name)) {
-        return sprintf(SHOUTING_GREET, $name);
+        return "HELLO {$name}!";
     }
 
-    return sprintf(GREET_TEMPLATE, $name);
+    return "Hello, {$name}.";
 }
 
 function isAllUppercase(string $name): bool
